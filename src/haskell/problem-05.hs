b3a74f4861109a21b0928004b20c45d854e9bf8d
Steven Testa <steve@testasoftware.com>
2015-10-19 10:13:51 -0400

    haskell problem-05

---
+++ src/haskell/problem-05.hs
@@ -0,0 +1,9 @@
+-- | 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+-- | What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+main :: IO()
+main = do 
+   -- | prime factorization
+    let answer = foldl (lcm) 1 [1..20]
+    print answer
+        
