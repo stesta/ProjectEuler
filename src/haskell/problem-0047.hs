a398e586885c7b80f20fe596b85c95d7361e3cb8
Steve Testa <steve@testasoftware.com>
2017-06-29 23:03:55 -0400

    problem 47 (haskell)

---
+++ src/haskell/problem-0047.hs
@@ -0,0 +1,26 @@
+-- The first two consecutive numbers to have two distinct prime factors are: 
+-- 14 = 2 × 7
+-- 15 = 3 × 5
+
+-- The first three consecutive numbers to have three distinct prime factors are: 
+-- 644 = 2^2 × 7 × 23
+-- 645 = 3 × 5 × 43
+-- 646 = 2 × 17 × 19.
+
+-- Find the first four consecutive integers to have four distinct prime factors. What is the first of these numbers?
+
+import Math.Primality
+import Data.List (nub)
+
+uniqueFactors :: Integer -> Int
+uniqueFactors = length . nub . primeFactors
+
+main :: IO ()
+main = do
+    let answer = head [n | n <- [2..] 
+                          , let n1 = uniqueFactors n
+                          , let n2 = uniqueFactors (n+1)
+                          , let n3 = uniqueFactors (n+2)
+                          , let n4 = uniqueFactors (n+3)
+                          , all (==4) [n1,n2,n3,n4]]
+    print answer
