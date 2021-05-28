4588ba4ebeb8614ce9794df12f0bf72c976599f9
Steve Testa <steve@testasoftware.com>
2021-05-28 13:38:06 -0400

    problem 59

--- src/problem-0059.hs
+++ src/problem-0059.hs
@@ -1,14 +1,16 @@
-
-
-import System.IO
+import System.IO ()
 import Data.Bits (xor)
+import Data.List (isInfixOf)
 
 main :: IO ()
 main = do
     cipher <- toByteArray <$> readFile "data/p059_cipher.txt"
-    let keys = [[x,y,z] | x <- ['a'..'z'], y <- ['a'..'z'], z <- ['a'..'z']]
-        decryptions = map (xor cipher) keys
-    print keys
+    let decryptions = [map toEnum $ zipWith xor cipher k | k <- keys] :: [[Char]]
+        winner = head $ filter (isInfixOf "Euler") decryptions -- originally used "the" and visually parsed through results
+    print $ sum . map fromEnum $ winner
+
+keys :: [[Int]]
+keys = [cycle . map fromEnum $ [x,y,z] | x <- ['a'..'z'], y <- ['a'..'z'], z <- ['a'..'z']]
 
 splitBy :: Char -> String -> [String]
 splitBy _ [] = []
@@ -16,7 +18,7 @@ splitBy c s  =
   let
     i = (length . takeWhile (/= c)) s
     (as, bs) = splitAt i s
-  in as : splitBy c (if bs == [] then [] else tail bs)
+  in as : splitBy c (if null bs then [] else tail bs)
 
 toByteArray :: String -> [Int]
 toByteArray xs = map read arr
